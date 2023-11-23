22e5aa6969dca78238f093454542c1fface7eb49
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-11-23 21:38:26 +0300

    styling

--- index.php
+++ index.php
@@ -18,27 +18,84 @@
 ?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Welcome to you WebApp</title>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/styles.css">
-  </head>
-  <body>
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            background-image: url('https://images.unsplash.com/photo-1591644302233-574b796e9d8f?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); /* Add the path to your background image */
+            background-size: cover;
+            background-position: center;
+            text-align: center;
+            margin: 0;
+            padding: 50px;
+        }
+
+        h1 {
+            font-size: 28px;
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        a {
+            text-decoration: none;
+            padding: 8px 16px;
+            background-color: #337ab7;
+            color: #fff;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        a:hover {
+            background-color: #23527c; 
+        }
+
+        .welcome-message {
+            font-size: 24px;
+            margin-bottom: 20px;
+            color: #fff; 
+        }
+
+        .user-email {
+            font-weight: bold;
+            color: #337ab7; 
+        }
+
+        .logout-link {
+            text-decoration: none;
+            padding: 8px 16px;
+            background-color: #337ab7;
+            color: #fff;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        .logout-link:hover {
+            background-color: #23527c; 
+        }
+    </style>
+</head>
+<body>
     <?php require 'partials/header.php' ?>
 
     <?php if(!empty($user)): ?>
-      <br> Welcome. <?= $user['email']; ?>
-      <br>You are Successfully Logged In
-      <a href="logout.php">
-        Logout
-      </a>
+        <div class="welcome-message">
+            Welcome, <span class="user-email"><?= $user['email']; ?></span><br>
+            You are Successfully Logged In
+        </div>
+        <a href="logout.php" class="logout-link">Logout</a>
     <?php else: ?>
-      <h1>Please Login or SignUp</h1>
-
-      <a href="login.php">Login</a> or
-      <a href="signup.php">SignUp</a>
+        <h1 style="color: white;">Please Login or SignUp</h1>
+        <a href="login.php" class="btn btn-primary">Login</a> or
+        <a href="signup.php" class="btn btn-secondary">SignUp</a>
     <?php endif; ?>
-  </body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
 </html>
